f7e872e919d1d1f606cdc981ab2a5a8c95d4d30d
Talal Mahmud <mahmudtalal2@gmail.com>
2025-10-15 01:19:26 +0600

    feat: updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-type Props = {};
-
-const loading = (props: Props) => {
-  return <div>loading...</div>;
-};
-
-export default loading;
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-[80vh] bg-gradient-to-br from-blue-900 via-green-800 to-black">
+      <div className="relative flex items-center justify-center">
+        <div className="h-20 w-20 rounded-full border-4 border-t-transparent border-primary animate-spin"></div>
+        <div className="absolute text-white font-semibold tracking-wide animate-pulse">
+          Loading...
+        </div>
+      </div>
+    </div>
+  );
+}
